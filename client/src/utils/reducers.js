3b0a37ecab637c73fbe52c0d13dd55e848c7c375
Mitchell Frey <mfcodingbc@gmail.com>
2022-07-07 23:36:58 -0400

    shopping cart added add and remove functions created

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -2,7 +2,13 @@ import { useReducer } from 'react';
 import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY
+  UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART
 } from './actions';
 
 export const reducer = (state, action) => {
@@ -26,6 +32,61 @@ export const reducer = (state, action) => {
       return {
         ...state,
         currentCategory: action.currentCategory
+      };
+
+    // if action type value is the value of `ADD_TO_CART`, return a new state object with an updated shopping cart array
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product]
+      };
+
+    // if action type value is the value of `ADD_MULTIPLE_TO_CART`, return a new state object with an updated shopping cart array
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products]
+      };
+
+    // if action type value is the value of `REMOVE_FROM_CART`, return a new state object with an updated shopping cart array
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState
+      };
+
+    // if action type value is the value of `UPDATE_CART_QUANTITY`, return a new state object with an updated shopping cart array
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        })
+      };
+
+    // if action type value is the value of `CLEAR_CART`, return a new state object with an updated shopping cart array that is empty
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: []
+      };
+
+    // if action type value is the value of `TOGGLE_CART`, return a new state object with an updated shopping cart state
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen
       };
 
     // if it's none of these actions, do not update state at all and keep things the same!
